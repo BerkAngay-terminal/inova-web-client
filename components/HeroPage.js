2ab51e7183a6fab0ed119924af50c2817d0b99a7
berk Angay <berkangay@blaccenterprises.com>
2023-02-22 21:53:21 +0300

    first stage init

---
+++ components/HeroPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroPage() {
+    return (
+        <div className=' flex items-center justify-center'>
+            <h1 className='text-6xl'>👋 Selam innova siteni geliştirmeye başladık.</h1>
+        </div>
+    )
+}
+
+export default HeroPage
